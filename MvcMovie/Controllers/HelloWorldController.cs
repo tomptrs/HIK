e2b843272032de64d8474b1d3b51c2a5655e30aa
kurtvdg <kurt.van.de.gaer@gmail.com>
2019-01-11 23:38:55 +0100

    MVC Movie project toegevoegd

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index()
+        {
+            return "This is a my default action...";
+        }
+
+        // Polymorfisme werkt niet in ASP.Net core, de reden is dat er mapping bestaat
+        //public string Welcome()
+        //{
+        //    return "This is the welcome action method...";
+        //}
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, number of times is: {numTimes}");
+        }
+
+        public IActionResult Kurt()
+        {
+            return View();
+        }
+
+        public IActionResult Test(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
